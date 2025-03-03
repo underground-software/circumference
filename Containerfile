underground-software/circumference

commit 6becb73f7be7ffb90efb5b0664fab406570f482a

upgrade fedora container and use virtualenv
Signed-off-by: Joel Savitz <joel@underground.software>

Containerfile
@@ -1,4 +1,4 @@
-FROM fedora:40
+FROM fedora:latest
 
 RUN dnf update -y && \
 	dnf install -y \
@@ -7,9 +7,8 @@ RUN dnf update -y && \
 	jq \
 	ShellCheck \
 	which \
-	python-flake8 \
-	python-pytest \
-	python-requests-unixsocket \
+	python-virtualenv \
+	python-pip \
 	git
 
 RUN sed -i 's/log_driver = "journald"/log_driver = "json-file"/' /usr/share/containers/containers.conf

start.sh
@@ -8,5 +8,7 @@ podman-compose build
 podman-compose up -d
 # wait until synapse is done initializing
 podman-compose logs -f submatrix 2>&1 | sed '/Synapse now listening on TCP port 8008/ q'
+virtualenv .
+pip install -r requirements.txt
 pytest
 podman-compose down

commit f82a42880cd9527cdfe5c5d5aaa704ddd61da83e

re-add incorrectly removed flake8
the listed commit removed flake8 (but not any of the other deps of the
test.sh script) so I have to assume it was a mistake. Re-add flake8 so
that test.sh continues to work.

Fixes: 6becb73 ("upgrade fedora container and use virtualenv")

Containerfile
@@ -7,6 +7,7 @@ RUN dnf update -y && \
 	jq \
 	ShellCheck \
 	which \
+	python-flake8 \
 	python-virtualenv \
 	python-pip \
 	git