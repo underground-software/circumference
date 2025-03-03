underground-software/circumference

commit 59ffb09b0ea69f65e4346315c153ec18408c8dcd

update Containerfile to use the new python tests
Signed-off-by: Joel Savitz <joel@underground.software>

start.sh
@@ -8,5 +8,5 @@ podman-compose build
 podman-compose up -d
 # wait until synapse is done initializing
 podman-compose logs -f submatrix 2>&1 | sed '/Synapse now listening on TCP port 8008/ q'
-./test.sh
+pytest
 podman-compose down

Containerfile
@@ -1,12 +1,15 @@
 FROM fedora:latest
 
-RUN dnf update && \
+RUN dnf update -y && \
 	dnf install -y \
 	podman \
 	podman-compose \
 	jq \
 	ShellCheck \
-	python3-flake8 \
+	which \
+	python-flake8 \
+	python-pytest \
+	python-requests-unixsocket \
 	git
 
 RUN sed -i 's/log_driver = "journald"/log_driver = "json-file"/' /usr/share/containers/containers.conf

commit 39da713a1b5b90a5e9c5fba9d3ed54a9b88bced0

support both new and old testing system
While the migration to pytest is still under consideration, we should
not break existing CI. We can check for the existence of test.sh in
singularity and call it if it exists, otherwise do pytest.

Fixes: 59ffb09 ("update Containerfile to use the new python tests")

start.sh
@@ -8,7 +8,12 @@ podman-compose build
 podman-compose up -d
 # wait until synapse is done initializing
 podman-compose logs -f submatrix 2>&1 | sed '/Synapse now listening on TCP port 8008/ q'
-virtualenv .
-pip install -r requirements.txt
-pytest
+if [ -f test.sh ]
+then
+	./test.sh
+else
+	virtualenv .
+	pip install -r requirements.txt
+	pytest
+fi
 podman-compose down