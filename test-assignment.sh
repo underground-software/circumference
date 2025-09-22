dea1ade901813951ec6a005f987732886382165c
Joel Savitz <joel@underground.software>
2025-09-22 13:55:08 -0400

    replace script with CI
    Signed-off-by: Joel Savitz <joel@underground.software>

--- test-assignment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# example script to quickly regenerate a test assignment
-
-cd "$(dirname "$0")"
-
-./denis/configure.sh remove -a kdlp_test
-./denis/configure.sh create -a kdlp_test -i "$(date -d '5 minutes' +%s)" -p "$(date -d '10 minutes' +%s)" -f "$(date -d '15 minutes' +%s)"
-./denis/configure.sh reload
